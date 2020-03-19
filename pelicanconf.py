3df0eca14145640e5a640ac89739d0fbce6b6276
Alysson Salgado <alysson.salgado@gauge.com.br>
2020-03-19 11:44:37 -0300

    deploy 2020-03-19

--- pelicanconf.py
+++ pelicanconf.py
@@ -35,12 +35,12 @@ SOCIAL = (('twitter', 'https://twitter.com/myprofile'),
           ('envelope','mailto:my@mail.address'))
 
 AUTHORS_BIO = {
-  "zutrinken": {
-    "name": "Zutrinken",
+  "alyssonrhuan": {
+    "name": "Alysson Rhuan",
     "cover": "https://arulrajnet.github.io/attila-demo/assets/images/avatar.png",
     "image": "https://arulrajnet.github.io/attila-demo/assets/images/avatar.png",
     "website": "http://blog.arulraj.net",
-    "location": "Chennai",
+    "location": "Brasil",
     "bio": "This is the place for a small biography with max 200 characters. Well, now 100 are left. Cool, hugh?"
   }
 }
@@ -50,4 +50,25 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
+
+
+# Post and Pages path
+ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}.html'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}.html'
 PAGE_URL = 'pages/{slug}/'
+PAGE_SAVE_AS = 'pages/{slug}/index.html'
+YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
+
+# Tags and Category path
+CATEGORY_URL = 'category/{slug}'
+CATEGORY_SAVE_AS = 'category/{slug}/index.html'
+CATEGORIES_SAVE_AS = 'catgegories.html'
+TAG_URL = 'tag/{slug}'
+TAG_SAVE_AS = 'tag/{slug}/index.html'
+TAGS_SAVE_AS = 'tags.html'
+
+# Author
+AUTHOR_URL = 'author/{slug}'
+AUTHOR_SAVE_AS = 'author/{slug}/index.html'
+AUTHORS_SAVE_AS = 'authors.html'
