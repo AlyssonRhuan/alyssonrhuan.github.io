52afd2330fb3797191db7279202026c3f3032686
Alysson Salgado <alysson.salgado@gauge.com.br>
2020-03-19 09:56:01 -0300

    DEPLOY 2020-03-19

---
+++ deploy.py
@@ -0,0 +1,13 @@
+import os
+from datetime import date
+
+commit_message = date.today()
+content = "pelican content -o output -s pelicanconf.py"
+output = "ghp-import output -b master"
+master = "git push origin master"
+commit = "git add . && git commit -m \"DEPLOY {}\"".format(str(date.today()))
+docs = "git push origin docs"
+
+command = 'cmd /k "{} && {} && {} && {} && {}"'.format(content, output, master, commit, docs)
+
+os.system(command)
--- pelicanconf.py
+++ pelicanconf.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+THEME = 'attila'
+
 AUTHOR = 'Alysson Rhuan'
 SITENAME = 'Galaxia de Dados'
 SITEURL = ''
